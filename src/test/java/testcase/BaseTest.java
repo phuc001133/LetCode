80a4110e89e08bc0558eb25ec79d2ff75519c669
Phuc Nguyen <phuc001133@gmail.com>
2023-07-27 19:43:35 +0700

    add Base class

--- src/test/java/testcase/BaseTest.java
+++ src/test/java/testcase/BaseTest.java
@@ -1,4 +1,26 @@
 package testcase;
 
+import core.configuration.Configuration;
+import core.datadriven.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+
 public class BaseTest {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        Configuration configuration = new Configuration("src/test/resources/app.properties");
+        driver = DriverFactory.getDriver(configuration.getProperty("browser"));
+        driver.get(configuration.getProperty("url"));
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
 }
